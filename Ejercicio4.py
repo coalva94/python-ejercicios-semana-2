5b5c2239b813c1bc1ea3807f08d224e50ec69299
Carlos <coalva.94@gmail.com>
2024-05-11 23:47:40 -0500

    Cuarto ejercicio resuelto

---
+++ Ejercicio4.py
@@ -0,0 +1,18 @@
+'''Crea un programa que pida al usuario ingresar un número del 1 al 7, e
+imprima el día de la semana correspondiente. Si el número está fuera de ese
+rango, mostrar un mensaje de error.'''
+
+semana = {
+    1:'Lunes',
+    2:'Martes',
+    3:'Miercoles',
+    4:'Jueves',
+    5:'Viernes',
+    6:'Sábado',
+    7:'Domingo'
+}
+num = int(input('Ingrese un numero del 1 al 7: '))
+if num in semana:
+    print(f'El dia correspondiente es: {semana[num]}')
+else:
+    print('El numero es incorrecto')
